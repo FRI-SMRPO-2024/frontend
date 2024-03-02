f5b7e0fbeb889a77fdd181a623d2b55c2870aa89
Gal Lindic <gal.lindic@easy.bi>
2024-03-02 13:50:14 +0100

    feat [SMRPO-30]: add generic Form components and login view

---
+++ src/components/Form/index.ts
@@ -0,0 +1,9 @@
+import BaseForm from "./BaseForm.vue";
+import FieldWrapper from "./FieldWrapper.vue";
+import InputField from "./InputField.vue";
+
+export {
+  BaseForm,
+  FieldWrapper,
+  InputField,
+};
